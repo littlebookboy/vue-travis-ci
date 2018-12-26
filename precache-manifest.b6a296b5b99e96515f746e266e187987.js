c3c657397f436492066668efe4fd5887412cc6d2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-26 12:52:36 +0000

    Deploy littlebookboy/vue-travis-ci to github.com/littlebookboy/vue-travis-ci.git:gh-pages

---
+++ precache-manifest.b6a296b5b99e96515f746e266e187987.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "39285ea4fdd46c4dc6c9",
+    "url": "/js/about.af146977.js"
+  },
+  {
+    "revision": "aac0a3582ea36d3a5c5b",
+    "url": "/css/app.b5342362.css"
+  },
+  {
+    "revision": "aac0a3582ea36d3a5c5b",
+    "url": "/js/app.0e8631ad.js"
+  },
+  {
+    "revision": "9562f49a44ac4a7992bc",
+    "url": "/js/chunk-vendors.a19961fb.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5135a271800686c6c3bc9730331d190d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
